48ba9e291301aa5021cf433ade2026ad8fa62252
kanak1901 <kanaksharma6925@gmail.com>
2026-07-23 23:34:23 +0530

    Added Procfile

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -27,7 +27,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    ...
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'job_portal.urls'
